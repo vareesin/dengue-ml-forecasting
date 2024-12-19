31c7d49e12a249a4fb048cd08d5a167d8c013208
vareesin <vareesadulyasas@outlook.com>
2024-12-19 23:03:02 +0000

    Setup for GitHub Pages deployment

--- dengue-dashboard/vite.config.ts
+++ dengue-dashboard/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/dengue-ml-forecasting/dengue-dashboard/',
+  base: '/dengue-ml-forecasting/', 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
